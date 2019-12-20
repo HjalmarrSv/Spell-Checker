63d17bf2300e7a371c0cfb26f2dde4c018550d7b
HjalmarrSv <58831450+HjalmarrSv@users.noreply.github.com>
2019-12-21 00:46:32 +0100

    åäö

--- SpellChecker.py
+++ SpellChecker.py
@@ -239,7 +239,7 @@ for i in range(5):
 # In[36]:
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-           'n','o','p','q','r','s','t','u','v','w','x','y','z',]
+           'n','o','p','q','r','s','t','u','v','w','x','y','z','å','ä','ö'] #å ä ö for some european languages
 
 def noise_maker(sentence, threshold):
     '''Relocate, remove, or add characters to create spelling mistakes'''
@@ -253,8 +253,8 @@ def noise_maker(sentence, threshold):
             noisy_sentence.append(sentence[i])
         else:
             new_random = np.random.uniform(0,1,1)
-            # ~33% chance characters will swap locations
-            if new_random > 0.67:
+            # ~33% chance characters will swap locations #25%
+            if new_random > 0.75:
                 if i == (len(sentence) - 1):
                     # If last character in sentence, it will not be typed
                     continue
@@ -263,12 +263,17 @@ def noise_maker(sentence, threshold):
                     noisy_sentence.append(sentence[i+1])
                     noisy_sentence.append(sentence[i])
                     i += 1
-            # ~33% chance an extra lower case letter will be added to the sentence
-            elif new_random < 0.33:
+            # ~33% chance an extra lower case letter will be added to the sentence #25%
+            elif new_random > 0.50:
                 random_letter = np.random.choice(letters, 1)[0]
                 noisy_sentence.append(vocab_to_int[random_letter])
                 noisy_sentence.append(sentence[i])
-            # ~33% chance a character will not be typed
+           # 25% chance of typical OCR/scanner error
+            elif new_random > 0.25:
+            #    random_letter = np.random.choice(letters, 1)[0]
+            #    noisy_sentence.append(vocab_to_int[random_letter])
+            #    noisy_sentence.append(sentence[i])                
+            # ~33% chance a character will not be typed #20%
             else:
                 pass     
         i += 1
